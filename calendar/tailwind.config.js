8b186a4aefc37ee4edb8f93e7fa490b7f07b23dd
AxSch <alex_schuneman@outlook.com>
2020-06-20 20:13:40 +0100

    Add custom styling for tailwind

---
+++ calendar/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    theme: {
+        extend: {
+            spacing: {
+                '72': '18rem',
+                '84': '21rem',
+            },
+            inset: {
+                '100': '100px',
+                '150': '150px',
+            },
+            colors: {
+                'lime-green': '#80ff80',
+                'cold-blue': '#66d9ff',
+            }
+        }
+    },
+}
