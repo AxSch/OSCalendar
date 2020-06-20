4304e30b7cfc142cee8f2d4c7759c4742dd6c810
AxSch <alex_schuneman@outlook.com>
2020-06-20 19:38:29 +0100

    Implement Calendar component
    add background styling(inline) to App

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
+import Calendar from './Components/Calendar'
+import winxp from './assets/images/winxp.jpg'
 
 function App() {
   return (
-    <div>
-      <div className="w-full max-w-md bg-gray-800">
-        test
-      </div>
+    <div className="relative flex justify-center h-screen" style={{ 
+        background: `url(${winxp}) no-repeat center center fixed`,
+        backgroundSize: 'cover'
+      }}>
+      <Calendar />
     </div>
   );
 }
